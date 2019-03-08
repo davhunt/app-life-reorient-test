7728573948656af09987ed845eac4fdf14af2bc4
David Hunt <davhunt@i16.red.uits.iu.edu>
2019-03-08 13:57:56 -0500

    neuro
    o Please enter the commit message for your changes. Lines starting

--- check_orientation.sh
+++ check_orientation.sh
@@ -3,5 +3,5 @@
 # make sure dwi is in 'RAS' orientation
 dwi=$(jq -r .dwi config.json)
 fslhd "$dwi" > dwi_header.txt
-[ "`grep -c Right-to-Left dwi_header.txt`" -gt "0" ] && fslswapdim "$dwi" -x y z dwi_fixed.nii.gz
-if [ -f dwi_fixed.nii.gz ]; then rm "$dwi" && mv dwi_fixed.nii.gz "$dwi"; fi
+[ "`grep -c Right-to-Left dwi_header.txt`" -gt "0" ] && fslorient -forceneurological "$dwi"
+#if [ -f dwi_fixed.nii.gz ]; then rm "$dwi" && mv dwi_fixed.nii.gz "$dwi"; fi
