ec9ccf9432914806e1c94cec23c947e5db2d062d
David Hunt <davhunt@i16.red.uits.iu.edu>
2019-03-08 14:10:42 -0500

    check orientation

--- check_orientation.sh
+++ check_orientation.sh
@@ -5,3 +5,4 @@ dwi=$(jq -r .dwi config.json)
 fslhd "$dwi" > dwi_header.txt
 [ "`grep -c Right-to-Left dwi_header.txt`" -gt "0" ] && fslorient -forceneurological "$dwi"
 #if [ -f dwi_fixed.nii.gz ]; then rm "$dwi" && mv dwi_fixed.nii.gz "$dwi"; fi
+fslhd "$dwi" > post_dwi_header.txt
